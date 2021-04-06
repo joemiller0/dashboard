d3078dbc6015ac1d064408e1b7544a28026fde6c
github username <Joemiller065@gmail.com>
2021-04-05 22:21:06 -0400

    some stlying

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -8,8 +8,8 @@ const Dashboard = () => {
             const clientId = process.env.REACT_APP_STRAVA_CLIENT_ID;
             const clientSecret = process.env.REACT_APP_STRAVA_CLIENT_SECRET;
             const refreshToken = process.env.REACT_APP_STRAVA_REFRESH_TOKEN;
-            const authUrl = "https://www.strava.com/oauth/token"
-            const refreshUrl = `${authUrl}?client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`
+            const authUrl = "https://www.strava.com/oauth/token";
+            const refreshUrl = `${authUrl}?client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
 
             fetch(refreshUrl, {
                 method: "POST",
@@ -22,6 +22,7 @@ const Dashboard = () => {
                 .then(tokenData => {
                     const activitiesUrl = "https://www.strava.com/api/v3/activities";
                     const getActivitiesUrl = `${activitiesUrl}?access_token=${tokenData.access_token}`;
+
                     fetch(getActivitiesUrl)
                         .then((res) => res.json())
                         .then((activities) => setActivities(activities));
--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -4,17 +4,24 @@ const WorkoutList = (props) => {
     const workouts = props.activities;
 
     return (
-        <ul className="workout-ul">
-            {workouts.map((a) => {
-                console.log(a)
-                const d = new Date(a.start_date)
-                const date = d.toDateString()
-                return <li key={a.id}>
-                            {a.name}<br />
-                            {date}
+        <div className="workout-container">
+            <ul className="workout-ul">
+                {workouts.map((a) => {
+                    console.log(a)
+                    const d = new Date(a.start_date)
+                    const date = d.toDateString()
+                    return (
+                        <li className="workout" key={a.id}>
+                        <div className="workout-header">
+                                <span className="workout-start-date">{date}</span>
+                        </div>
+                                <span className="workout-name">{a.name}</span>
                         </li>
-            })}
-        </ul>
+
+                    )
+                })}
+            </ul>
+        </div>
     );
 }
 
