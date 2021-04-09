d9efa2af60e435a0db57225d807bbf90b4a69f77
github username <Joemiller065@gmail.com>
2021-04-09 17:08:15 -0400

    some more wrasslin with it, not quite yet

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -15,50 +15,18 @@ const WorkoutList = (props) => {
         }
     })
 
-    console.log(strava)
-    console.log(whoop)
+    const createWorkoutArr = () => {
 
-    const combineDates = () => {
-        for (let s = 0; s < strava.length; s++) {
-            const d = new Date(strava[s].start_date)
-            const date = d.toDateString()
-
-            for (let w = 0; w < whoop.length; w++) {
-                if (strava[s].start_date === whoop[w].start_date) {
-
-
-                    workouts.push(
-                        <div className="workout" key={strava[s].id}>
-                            <div className="workout-header">
-                                <span className="workout-start-date">{date}</span>
-                            </div>
-                            <div className="workouts">
-                                <p>{strava[s].name}</p>
-                                <p>{whoop[w].name}</p>
-                            </div>
-                        </div>
-                    )
-                } else {
-                    workouts.push(
-                        <div className="workout" key={whoop[w].id}>
-                            <div className="workout-header">
-                                <span className="workout-start-date">{date}</span>
-                            </div>
-                            <div className="workouts">
-                                <p>{strava[s].name}</p>
-                            </div>
-                        </div>
-                    )
-                }
-            }
-        }
     }
 
-    //this is all wrong above, there will be whoop data where these is not strava stat at times, not vice versa. its early woof
+
+    console.log(strava)
+    console.log(whoop)
+    console.log(workouts)
+
     return (
         <div className="workout-container">
-
-            {combineDates()}
+            {}
         </div>
     );
 }
