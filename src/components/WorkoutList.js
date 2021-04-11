2738ff31a23437bdf2b9e7ca18d82b12772e4825
github username <Joemiller065@gmail.com>
2021-04-11 10:06:13 -0400

    got all the dates now we can build the workouts

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,46 +1,54 @@
 import { useEffect, useState } from "react";
-import '../stylesheets/workout.css';
+import "../stylesheets/workout.css";
 
 const WorkoutList = (props) => {
+    const activities = props.activities;
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
-    const activities = props.activities;
 
-    let whoop = []
-    let strava = []
-    let workouts = []
+    let whoop = [];
+    let strava = [];
+    let workouts = [];
 
     activities.forEach((a) => {
-        if (a.name[0] === 'W') {
-            whoop.push(a)
+        if (a.name[0] === "W") {
+            whoop.push(a);
         } else {
-            strava.push(a)
+            strava.push(a);
         }
-    })
+    });
 
     // set startDate and endDate based on first and last item in the activities array
-    useEffect(()=>{
-        const latest = activities[0]
-        const earliest = activities[activities.length-1]
+    useEffect(() => {
+        const latest = activities[0];
+        const earliest = activities[activities.length - 1];
 
-        if(latest !== undefined) {
-            const endDate = new Date(latest.start_date)
-            setEndDate(endDate.toDateString())
+        if (latest !== undefined) {
+            setEndDate(new Date(latest.start_date));
         }
-        if(earliest !== undefined) {
-            const startDate = new Date(earliest.start_date)
-            setStartDate(startDate.toDateString())
+        if (earliest !== undefined) {
+            setStartDate(new Date(earliest.start_date));
         }
 
-    }, [activities])
+    }, [activities]);
+
+    if(startDate !== ''){
+        const year = startDate.getFullYear();
+        const month = startDate.getMonth();
+        let day = startDate.getDate();
+        const dates = [startDate];
     
+        while (dates[dates.length - 1] < endDate) {
+            dates.push(new Date(year, month, ++day));
+        }
+        console.log(dates)
+    }
+
     console.log(startDate)
-    console.log(endDate)
-    return (
-        <div className="workout-container">
-            {}
-        </div>
-    );
-}
+    console.log(activities)
+
+
+    return <div className="workout-container">{ }</div>;
+};
 
 export default WorkoutList;
