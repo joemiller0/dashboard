d5f228b60c7158139352a6dcc8e015154463185a
github username <Joemiller065@gmail.com>
2021-04-13 18:33:53 -0400

    moving logic to a new component structure

---
+++ src/components/Workout.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "../stylesheets/workout.css";
+
+const Workout = (props) => {
+
+    return (
+        <div className="workout">
+            <p>test</p>
+        </div>);
+};
+
+export default Workout;
--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,63 +1,55 @@
-import { useEffect, useState } from "react";
-import "../stylesheets/workout.css";
+import { useState } from "react";
+import Workout from './Workout';
 
 const WorkoutList = (props) => {
     const activities = props.activities;
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
-    const [headerDates, setHeaderDates] = useState([]);
+    // const [startDate, setStartDate] = useState('');
+    // const [endDate, setEndDate] = useState('');
+    // const [headerDates, setHeaderDates] = useState([]);
 
-    // let whoop = [];
-    // let strava = [];
+    let whoop = [];
+    let strava = [];
     // let workouts = [];
 
-    // activities.forEach((a) => {
-    //     if (a.name[0] === "W") {
-    //         whoop.push(a);
-    //     } else {
-    //         strava.push(a);
-    //     }
-    // });
+    activities.forEach((a) => {
+        if (a.name[0] === "W") {
+            whoop.push(a);
+        } else {
+            strava.push(a);
+        }
+    });
 
     // set startDate and endDate based on first and last item in the activities array
-    useEffect(() => {
-        const latest = activities[0];
-        const earliest = activities[activities.length - 1];
+    // useEffect(() => {
+    //     const latest = activities[0];
+    //     const earliest = activities[activities.length - 1];
 
-        if (latest !== undefined) {
-            setEndDate(new Date(latest.start_date));
-        }
-        if (earliest !== undefined) {
-            setStartDate(new Date(earliest.start_date));
-        }
+    //     if (latest !== undefined) {
+    //         setEndDate(new Date(latest.start_date));
+    //     }
+    //     if (earliest !== undefined) {
+    //         setStartDate(new Date(earliest.start_date));
+    //     }
 
-    }, [activities]);
+    // }, [activities]);
 
-    if(startDate !== ''){
-        const year = startDate.getFullYear();
-        const month = startDate.getMonth();
-        let day = startDate.getDate();
-        const dates = [startDate];
+    // if(startDate !== ''){
+    //     const year = startDate.getFullYear();
+    //     const month = startDate.getMonth();
+    //     let day = startDate.getDate();
+    //     const dates = [startDate];
     
-        while (dates[dates.length - 1] < endDate) {
-            dates.push(<p className="workout-start-date">{new Date(year, month, ++day)}</p>);
-        }
-        // setHeaderDates(dates)
-        console.log(dates)
-    }
+    //     while (dates[dates.length - 1] < endDate) {
+    //         dates.push(<p className="workout-start-date">{new Date(year, month, ++day)}</p>);
+    //     }
+    //     // setHeaderDates(dates)
+    //     console.log(dates)
+    // }
 
 
     return (
         <div className="workout-container">
-            {activities.map((a)=>{
-                <div key={a.id} className="workout">
-                        {headerDates.map((d)=>{
-                            <div classaName="workout-header">
-                                {d}
-                            </div>
-                        })}
-                </div>
-            })}
+            <Workout strava={strava} whoop={whoop}/>
         </div>);
 };
 
