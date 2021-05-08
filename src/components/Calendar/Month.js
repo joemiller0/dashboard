835ca76e871a95321c9f29cda3661f35de3a7026
github username <Joemiller065@gmail.com>
2021-05-08 08:12:15 -0400

    engineering the calendar

--- src/components/Calendar/Month.js
+++ src/components/Calendar/Month.js
@@ -22,9 +22,17 @@ const Month = ({ monthName, currentDate }) => {
         return <th key={day}>{day}</th>;
     });
 
+    const monthIndex = currentDate.getMonth();
+    const firstDayIndex = new Date(year, monthIndex).getDay();
+
+    //looking for a day component to populate in every square on the table, greyed out for past and future months in the first and last week of the calendar.
+    //going to need to know about the last and next month to populate days correctly. 
+    //each day should have its own full date and be mapped correctly to that space on the calendar.
+
     return (
         <div className="month">
             <h3>{monthName} {year}</h3>
+            <p>{fullDays[dayIndex]}</p>
             <table>
                 <thead>
                     <tr>{dayNames}</tr>
