e6a1e0cfed6c6ab8eb72f6ad1bbf8674c3082af9
github username <Joemiller065@gmail.com>
2021-05-11 06:02:05 -0400

    WeeksContainer renamed

--- src/components/Calendar/Month.js
+++ src/components/Calendar/Month.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import uniqid from "uniqid";
-import Week from './Week';
+import WeeksContainer from './WeeksContainer';
 
 const Month = ({ activities, monthName, currentDate }) => {
     const [workouts, setWorkouts] = useState({})
@@ -29,21 +29,22 @@ const Month = ({ activities, monthName, currentDate }) => {
         "Friday",
         "Saturday",
     ];
+    const daysInMonth = (month, year) => {
+        return new Date(year, month, 0).getDate();
+    }
+    const dayNames = fullDays.map(day => {
+        return <th key={day}>{day}</th>;
+    });
     // const date = currentDate.getDate();
     const dayIndex = currentDate.getDay();
     const monthIndex = currentDate.getMonth();
     const year = currentDate.getFullYear();
     const firstDayIndex = new Date(year, monthIndex).getDay();
+    const totalDays = daysInMonth(monthIndex+1, year);
 
-    
-    
     //looking for a day component to populate in every square on the table
-    //going to need to know about the last and next month to populate days correctly. 
     //each day should have its own full date and be mapped correctly to that space on the calendar.
     
-    const dayNames = fullDays.map(day => {
-        return <th key={day}>{day}</th>;
-    });
     return (
         <div className="month">
             <h3>{monthName} {year}</h3>
@@ -53,9 +54,13 @@ const Month = ({ activities, monthName, currentDate }) => {
                     <tr>{dayNames}</tr>
                 </thead>
                 <tbody>
-                    
-                    <Week key={uniqid()} workouts={workouts} workoutDates={workoutDates} firstDayIndex={firstDayIndex}/>
-                
+                    <WeeksContainer 
+                        key={uniqid()} 
+                        workouts={workouts} 
+                        workoutDates={workoutDates} 
+                        firstDayIndex={firstDayIndex}
+                        totalDays={totalDays}
+                    />
                 </tbody>
             </table>
         </div>
---
+++ src/components/Calendar/WeeksContainer.js
@@ -0,0 +1,18 @@
+// import { useEffect, useState } from "react";
+// import "../stylesheets/workout.css";
+
+const WeeksContainer = ({workouts, workoutDates, firstDayIndex}) => {
+
+
+
+    return (
+        <tr className="WeeksContainer">
+            <td>
+                <p>WeeksContainer.js</p>
+            </td>
+        </tr>
+    );
+};
+
+export default WeeksContainer;
+
