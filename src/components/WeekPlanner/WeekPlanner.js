d7cf0db8f6b0d943aa87d5e07ffbc8e59e717b69
github username <Joemiller065@gmail.com>
2021-09-19 17:41:48 -0400

    a little clean up

--- src/components/WeekPlanner/WeekPlanner.js
+++ src/components/WeekPlanner/WeekPlanner.js
@@ -3,44 +3,9 @@ import { useState, useEffect } from "react";
 import uniqid from "uniqid";
 import TimeSlotContainer from './TimeSlotContainer';
 
-
-
 const WeekPlanner = ({workouts}) => {
-    // const [amWorkouts, setAMWorkouts] = useState([]);
-    // const [lunchWorkouts, setLunchWorkouts] = useState([]);
-    // const [pmWorkouts, setPMWorkouts] = useState([]);
 
-        // useEffect(()=>{
-        //     const am =[];
-        //     const lunch = []
-        //     const pm = []
-        //     workouts.map((w)=>{
-        //         if(w.time ==='am'){
-        //             am.push()
-        //             setAMWorkouts(am)
-        //         }
-        //         if(w.time ==='lunch'){ 
-        //             lunch.push(w)
-        //             setLunchWorkouts(lunch)
-        //         }
-        //         if(w.time ==='pm'){
-        //             pm.push(w)
-        //             setPMWorkouts(pm)
-        //         }
-        //         return null;
-        //         // these are going to be arrays of TimeSlots, maybe better to think of them as rows.
-        //         // will need to insert at a partiuclar index based on day and fill in slots with empty timeslots  
-
-        //     })
-
-
-        // }, [workouts])
-
-
-        // console.log(amWorkouts)
-        // // console.log(lunchWorkouts)
-        // // console.log(pmWorkouts)
-        console.log(workouts)
+    console.log(workouts)
     const abrevDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     const dayNames = abrevDays.map((day) => {
         return <td key={day}>{day}</td>;
@@ -57,7 +22,7 @@ const WeekPlanner = ({workouts}) => {
                     </thead>
 
                     <TimeSlotContainer workouts={workouts}/>
-                    
+
                 </table>
 
         </div>
