ce8570e36e89736a861d8c4feb22c38f21bcd15b
github username <Joemiller065@gmail.com>
2021-10-22 14:59:31 -0400

    state added for the days in the am slots, this is athe first try at this.

--- src/components/WeekPlanner/AMSlots.js
+++ src/components/WeekPlanner/AMSlots.js
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
 const AMSlots = (workouts) => {
+    const [sunday, setSunday] = useState("sunday");
+    const [monday, setMonday] = useState("monday");
+    const [tuesday, setTuesday] = useState("tuesday");
+    const [wednesday, setWednesday] = useState("wednesday");
+    const [thursday, setThursday] = useState("thursday");
+    const [friday, setFriday] = useState("friday");
+    const [saturday, setSaturday] = useState("saturday");
+
+
+    useEffect(()=>{
+
+
+
+    }, [workouts, sunday])
+    console.log(sunday)
+
     return(
         <tr>
-            <td>am</td>
+            <td>am</td><td>{sunday}</td><td>{monday}</td><td>{tuesday}</td><td>{wednesday}</td><td>{thursday}</td><td>{friday}</td><td>{saturday}</td>
         </tr>
     )
 };
