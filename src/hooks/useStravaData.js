eda02815d3c4bd70d7de150bb3b98c8b3aa9d0b7
github username <Joemiller065@gmail.com>
2021-10-29 17:14:50 -0400

    process of log building

--- src/hooks/useStravaData.js
+++ src/hooks/useStravaData.js
@@ -1,68 +1,84 @@
 import { useState, useEffect } from "react";
 
 function useStravaData() {
-    const [stravaLogs, SetStravaLogs] = useState({});
+    const [stravaLogs, SetStravaLogs] = useState([]);
     const [athlete, setAthlete] = useState({});
 
     useEffect(() => {
         const clientId = process.env.REACT_APP_STRAVA_CLIENT_ID;
         const clientSecret = process.env.REACT_APP_STRAVA_CLIENT_SECRET;
         const refreshToken = process.env.REACT_APP_STRAVA_REFRESH_TOKEN;
-        if (!clientId || !clientSecret) return
+        if (!clientId || !clientSecret) return;
         const refreshUrl = `https://www.strava.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
 
         fetch(refreshUrl, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Accept": "application/json"
+                Accept: "application/json",
             },
         })
-            .then(res => res.json())
-            .then(tokenData => {
+            .then((res) => res.json())
+            .then((tokenData) => {
                 const activitiesUrl = `https://www.strava.com/api/v3/activities?access_token=${tokenData.access_token}`;
                 const athleteUrl = `https://www.strava.com/api/v3/athlete?access_token=${tokenData.access_token}`;
                 fetch(athleteUrl)
-                    .then((res)=> res.json())
-                    .then(athlete => setAthlete(athlete))
+                    .then((res) => res.json())
+                    .then((athlete) => setAthlete(athlete));
 
                 fetch(activitiesUrl)
                     .then((res) => res.json())
-                    .then(activities => {
+                    .then((activities) => {
+                        const localLogsURL = "http://localhost:5000/logs";
 
-                        const localLogsURL = "http://localhost:5000/logs" 
-                        fetch(localLogsURL)
-                            .then((res)=>res.json())
-                            .then(localLogs => {
-                                console.log(localLogs)
-                                console.log(activities)
+                        activities.forEach(log=>{
 
-                                const allLogs = [...localLogs, ...activities];
-                                console.log(allLogs)
+                            console.log(log)
 
-                                let stravaLogsObj = {}
-                                activities.forEach((stravaLog) => {
-                                    const date = stravaLog.start_date_local.split('T')[0];
-                                    if (!stravaLogsObj[date]) {
-                                        stravaLogsObj[date] = [stravaLog]
-                                    } else {
-                                        stravaLogsObj[date].push(stravaLog)
-                                    }
-                                })
-                                SetStravaLogs(stravaLogsObj)
-                                // setDates(Object.keys(stravaLogsObj))
-                            })
+                            // fetch(localLogsURL, {
+                            //     method: "POST",
+                            //     headers: {
+                            //         "Content-Type": "application/json",
+                            //         Accept: "application/json",
+                            //     },
+                            //     body: JSON.stringify(log)
+                            // })
+                            //     .then(res=>res.json())
+                            //     .then(logs=>console.log(logs))
+
+                        })
 
 
+                        // fetch(localLogsURL)
+                        //     .then((res)=>res.json())
+                        //     .then(localLogs => {
+                        //         console.log(localLogs)
+                        //         console.log(activities)
 
+                        //         const allLogs = [...localLogs, ...activities];
+                        //         console.log(allLogs)
+
+                        //     })
+
+                        // let stravaLogsObj = {}
+                        // activities.forEach((stravaLog) => {
+                        //     const date = stravaLog.start_date_local.split('T')[0];
+                        //     if (!stravaLogsObj[date]) {
+                        //         stravaLogsObj[date] = [stravaLog]
+                        //     } else {
+                        //         stravaLogsObj[date].push(stravaLog)
+                        //     }
+                        // })
+                        // SetStravaLogs(stravaLogsObj)
+                        // // setDates(Object.keys(stravaLogsObj))
+                        // console.log(stravaLogs)
                     });
-            })
+            });
     }, []);
-
 
     return {
         stravaLogs,
-        athlete, 
+        athlete,
     };
 }
 
