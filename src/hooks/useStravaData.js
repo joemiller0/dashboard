7ce58e27dc6b03763977a8ed29b2e8237dfdf2c9
github username <Joemiller065@gmail.com>
2021-10-31 10:22:00 -0400

    dupe records fixed by lining up strava ids with lid

--- src/hooks/useStravaData.js
+++ src/hooks/useStravaData.js
@@ -9,53 +9,41 @@ function useStravaData() {
         const clientSecret = process.env.REACT_APP_STRAVA_CLIENT_SECRET;
         const refreshToken = process.env.REACT_APP_STRAVA_REFRESH_TOKEN;
         if (!clientId || !clientSecret) return;
-        const refreshUrl = `https://www.strava.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
 
-        fetch(refreshUrl, {
+        fetch(`https://www.strava.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 Accept: "application/json",
             },
         })
-            .then((res) => res.json())
-            .then((tokenData) => {
-                const activitiesUrl = `https://www.strava.com/api/v3/activities?access_token=${tokenData.access_token}`;
-                const athleteUrl = `https://www.strava.com/api/v3/athlete?access_token=${tokenData.access_token}`;
-                fetch(athleteUrl)
-                    .then(res => res.json())
-                    .then(athlete => setAthlete(athlete));
-
-                fetch(activitiesUrl)
+            .then(res => res.json())
+            .then(tokenData => {
+                fetch(`https://www.strava.com/api/v3/athlete?access_token=${tokenData.access_token}`)
+                .then(res => res.json())
+                .then(athlete => setAthlete(athlete));
+                
+                fetch(`https://www.strava.com/api/v3/activities?access_token=${tokenData.access_token}`)
                     .then(res => res.json())
                     .then(activities => {
-                        // const idCheck = [];
                         activities.forEach( log => {
 
-                            // if (log.upload_id === null) {
-                            //     const mid = log.map.id.substring(1);
-                            //     idCheck.push(parseInt(mid))
-                            // } else {
-                            //     idCheck.push(log.upload_id)
-                            // }
-                            
-                            fetch("http://localhost:5000/logs", {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                    Accept: "application/json",
-                                },
-                                body: JSON.stringify({
-                                    "lid": log.upload_id,
-                                    "upload_id": log.upload_id,
-                                    "body": null,
-                                    "date": null, 
-                                    "time": null,
-                                    "stravaLog": log
+                                fetch("http://localhost:5000/logs", {
+                                    method: "POST",
+                                    headers: {
+                                        "Content-Type": "application/json",
+                                        Accept: "application/json",
+                                    },
+                                    body: JSON.stringify({
+                                        "lid": log.id,
+                                        "body": null,
+                                        "date": null, 
+                                        "time": null,
+                                        "stravaLog": log
+                                    })
                                 })
-                            })
-                                .then(res=>res.json())
-                                .then(logs=>console.log(logs))
+                                    .then(res => res.json())
+                                    .then(logs => console.log(logs))
                         })
 
                         // fetch("http://localhost:5000/logs")
